e3e4c1452e9fa3e486d4f7e9e2e86d47bff1ba7b
mitchell <mitchellwortsman@gmail.com>
2018-01-16 00:04:55 -0500

    fixed some plotting stuff

--- plotutil/plotutil.go
+++ plotutil/plotutil.go
@@ -4,7 +4,8 @@ import (
 	"github.com/mwortsma/particle_systems/probutil"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/plotutil"
+	//"gonum.org/v1/plot/plotutil"
+	"image/color"
 	"gonum.org/v1/plot/vg"
 	"sort"
 )
@@ -27,25 +28,40 @@ func getSortedKeys(distr probutil.Distr) []string {
     return keys
 }
 
-func PlotDistr(distr probutil.Distr) {
+func getColors(n int) []color.RGBA {
+	full := []color.RGBA{color.RGBA{R: 255, G: 0, B: 0, A:255}, color.RGBA{R: 0, G: 255, B: 155, A:155}, color.RGBA{R: 0, G: 255, B: 255, A:255}}
+	return full[:n]
+}
+
+func PlotDistr(title string, distributions []probutil.Distr, labels []string) {
 	p, err := plot.New()
 	if err != nil {
 		panic(err)
 	}
 
-	p.Title.Text = "Plotutil example"
-	p.X.Label.Text = "X"
-	p.Y.Label.Text = "Y"
+	colors := getColors(len(distributions))
 
-	sortedKeys := getSortedKeys(distr)
-	err = plotutil.AddLinePoints(p,
-		"First", getPoints(distr, sortedKeys))
-	if err != nil {
-		panic(err)
+	p.Title.Text = title
+	p.X.Label.Text = "Path"
+	p.Y.Label.Text = "Probability"
+
+	sortedKeys := getSortedKeys(distributions[0])
+	for i, v := range(distributions) {
+
+		l, err := plotter.NewLine(getPoints(v, sortedKeys))
+		if err != nil {
+			panic(err)
+		}
+		l.LineStyle.Width = vg.Points(1)
+		l.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
+		l.LineStyle.Color = colors[i]
+		p.Add(l)
+		p.Legend.Add(labels[i], l)
 	}
 
+
 	// Save the plot to a PNG file.
-	if err := p.Save(4*vg.Inch, 4*vg.Inch, "points.png"); err != nil {
+	if err := p.Save(10*vg.Inch, 10*vg.Inch, "points.png"); err != nil {
 		panic(err)
 	}
 }
